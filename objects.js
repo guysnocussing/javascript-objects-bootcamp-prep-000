053fc3b3a6951ca4ac31c4ec37487643c75e9f75
JONATHAN EASTIN <j.d.east@icloud.com>
2017-12-20 03:18:12 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,7 @@ function updatePlaylist(obj, key, value) {
   return Object.assign({}, obj, { [key]: value });
 }
 
-function removeFromPlaylist(obj, key, value) {
-   delete obj[key] = value;
+function removeFromPlaylist(obj, key) {
+  delete obj[key];
+  return obj;
 }
