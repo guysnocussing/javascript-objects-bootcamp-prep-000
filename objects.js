15e3d72fa6d00316d791c5abee46d76a3a13e1e8
JONATHAN EASTIN <j.d.east@icloud.com>
2017-12-20 02:56:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = { artistName : songTitle };
+var playlist = { 'Led Zepplin' : 'Underfoot' };
 
 function updatePlaylist(obj, key, value) {
   return Object.assign({}, obj, { [key]: value });
